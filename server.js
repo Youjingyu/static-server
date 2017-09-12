7b4a4c31f7b15184e44fa184d296ba5ef8d7044d
youjingyu <735284268@qq.com>
2017-09-12 16:19:26 +0800

    change https to default port

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use("/", express.static(__dirname + '/'));
 http.createServer(app).listen('8090', function() {
     console.log('启动http服务器完成');
 });
-https.createServer(httpsOptions, app).listen('8091', function() {
+https.createServer(httpsOptions, app).listen('443', function() {
     console.log('启动https服务器完成');
 });
