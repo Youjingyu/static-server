a6d87adeb5895580606f981f5e9d34ac9fb49609
youjingyu <735284268@qq.com>
2017-10-10 10:27:49 +0800

    refactor to pure static server

--- static/js/16.417b4ce1d7a0e5af03a5.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([16],{144:function(n,e,t){var r=t(53)(null,t(251),null,null,null);n.exports=r.exports},251:function(n,e){n.exports={render:function(){var n=this,e=n.$createElement;n._self._c;return n._m(0)},staticRenderFns:[function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("div",{staticStyle:{padding:"100px 0","text-align":"center"}},[n._v("\n    此页面暂未开发\n  ")])])}]}}});
-//# sourceMappingURL=16.417b4ce1d7a0e5af03a5.js.map
